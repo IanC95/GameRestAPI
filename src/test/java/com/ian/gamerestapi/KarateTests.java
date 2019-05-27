e10d3fc1c9885c76f861c60318f31d22aa33c3e7
IanC95 <ian.cox.1995@gmail.com>
2019-05-27 14:17:53 +0100

    Change KarateTests.java to start application

--- src/test/java/com/ian/gamerestapi/KarateTests.java
+++ src/test/java/com/ian/gamerestapi/KarateTests.java
@@ -1,8 +1,15 @@
 package com.ian.gamerestapi;
 
 import com.intuit.karate.junit4.Karate;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
 
 @RunWith(Karate.class)
 public class KarateTests {
+
+    @BeforeClass
+    public static void init(){
+        SpringApplication.run(GameApiApplication.class, "");
+    }
 }
